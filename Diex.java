847b9e4b6d32834343dd4e72167c9257c0fcf6c5
Samuel Rivera <samuel.rivera@mrjaffesclass.com>
2023-11-28 11:09:46 -0800

    diex without roll method

--- Diex.java
+++ Diex.java
@@ -2,24 +2,28 @@ import java.util.Scanner;
 import java.util.InputMismatchException;
 public class Diex
 {
-     private int value;
-   
+     private int value = 1;    
     public void roll(int max){
-        Scanner sideScan = new Scanner(System.in);
+        value = (int)(Math.random()*max) + 1;
+    }
+   
+     public Diex(){
+        Scanner sideScan = new Scanner(System.in); 
+        int max = 0;
         while(true){
             System.out.println("input the number of sides you want");
             try{
                 max = sideScan.nextInt();
                 value = (int)(Math.random()*max) + 1;
+                System.out.println(getValue());
             }catch (InputMismatchException error) {
                System.out.println("please enter an integer");
                 sideScan.next();
            }
         }
+        
     }
-     public Diex(){
-        roll();
-    }
+
     public int getValue(){
         return value;
     }
