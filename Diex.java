66754f497ed257634aad4e72bef83a77bca0e4af
Samuel Rivera <samuel.rivera@mrjaffesclass.com>
2023-11-29 11:27:09 -0800

    rollcode

--- Diex.java
+++ Diex.java
@@ -2,26 +2,15 @@ import java.util.Scanner;
 import java.util.InputMismatchException;
 public class Diex
 {
-     private int value = 1;    
-    public void roll(int max){
+     private int value = 1;  
+     private int max = 6;
+    public void roll(){
         value = (int)(Math.random()*max) + 1;
     }
    
-     public Diex(){
-        Scanner sideScan = new Scanner(System.in); 
-        int max = 0;
-        while(true){
-            System.out.println("input the number of sides you want");
-            try{
-                max = sideScan.nextInt();
-                value = (int)(Math.random()*max) + 1;
-                System.out.println(getValue());
-            }catch (InputMismatchException error) {
-               System.out.println("please enter an integer");
-                sideScan.next();
-           }
-        }
-        
+     public Diex(int sides){
+         this.max = sides;
+         this.roll();
     }
 
     public int getValue(){
